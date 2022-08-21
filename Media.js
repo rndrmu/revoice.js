34961b95710078b318589c1e88476cb9ae81954e
rndrmu <jhallwachs36@gmail.com>
2022-08-21 12:30:10 +0200

    a

--- Media.js
+++ Media.js
@@ -28,7 +28,7 @@ class Media {
   }
   spawnFFmpeg(input, port = 5030) {
     return require("child_process").spawn("ffmpeg", [
-      "-re", "-i", input, "-map", "0:a", "-b:a", "48k", "-maxrate", "48k", "-c:a", "libopus", "-f", "rtp", "rtp://127.0.0.1:" + port
+      "-re", "-i", input, "-map", "0:a", "-b:a", "48k", "-maxrate", "48k", "-c:a", "libopus", "-f", "rtp", "rtp://127.0.0.1:5030"
     ])
   } 
 
